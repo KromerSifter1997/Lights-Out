b7cd734ec657521182791a1ac95f3bca78d7cbca
KromerSifter1997 <93610952+KromerSifter1997@users.noreply.github.com>
2023-04-10 01:06:13 -0500

    Add files via upload

--- app/src/main/java/com/example/lightsout/MainActivity.java
+++ app/src/main/java/com/example/lightsout/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.lightsout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mLightOnColorId;
     private LightsOutGame mGame;
     private GridLayout mLightGrid;
     private int mLightOnColor;
@@ -23,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLightOnColorId = R.color.yellow;
 
         mLightGrid = findViewById(R.id.light_grid);
 
@@ -92,4 +101,35 @@ public class MainActivity extends AppCompatActivity {
     public void onNewGameClick(View view) {
         startGame();
     }
+    public void onHelpClick(View view) {
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
+    }
+
+    public void onChangeColorClick(View view) {
+        // Send the current color ID to ColorActivity
+        Intent intent = new Intent(this, ColorActivity.class);
+        intent.putExtra(ColorActivity.EXTRA_COLOR, mLightOnColorId);
+        mColorResultLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> mColorResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+
+                            int colorId = data.getIntExtra(ColorActivity.EXTRA_COLOR, R.color.yellow);
+
+                            // Create the "on" button color from the chosen color ID from ColorActivity
+                            mLightOnColorId = data.getIntExtra(ColorActivity.EXTRA_COLOR, R.color.yellow);
+                            mLightOnColor = ContextCompat.getColor(MainActivity.this, mLightOnColorId);
+                            setButtonColors();
+                        }
+                    }
+                }
+            });
 }
